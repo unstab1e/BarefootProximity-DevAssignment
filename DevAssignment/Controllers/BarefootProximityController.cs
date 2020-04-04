943ceb55bd7eaabebe898573f2c91767129c4e23
Thomas M. Grable <grablet@hotmail.com>
2020-04-04 07:12:07 -0400

    Basic functionality is complete. Need to work on tests.

---
+++ DevAssignment/Controllers/BarefootProximityController.cs
@@ -0,0 +1,91 @@
+﻿using DevAssignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevAssignment.Controllers
+{
+    public class BarefootProximityController : Controller
+    {
+        // GET: BarefootProximity
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Parses the given BarefootProximityModel's comma delimited SearchList into individual Items.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public BarefootProximityModel PopulateItemsList(BarefootProximityModel model)
+        {
+            // Model's SearchList contains a value?
+            if (null != model.SearchList && !string.IsNullOrEmpty(model.SearchList))
+            {
+                // Yes - Create string array from SearchList.
+                string[] items = model.SearchList.Split(',');
+                // Cycle through items in array to create Items.
+                for (int i = 0; i < items.Length; i++)
+                {
+                    model.Items.Add(items[i].Trim());
+                }
+            }
+
+            // Return given model.
+            return model;
+        }
+
+
+        /// <summary>
+        /// Procedure called from index page via a form submit.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public ActionResult ProcessSearchList(BarefootProximityModel model)
+        {
+            // Given model state valid?
+            if (ModelState.IsValid)
+            {
+                // Yes - Populate Items List.
+                model = PopulateItemsList(model);
+
+                Session["BarefootProximityItems"] = model.Items;
+                // Redirect to Search page.
+                return View("/Views/BarefootProximity/Search.cshtml", model);
+            }
+            else
+            {
+                // No - Remain on index page.
+                return View("/");
+            }
+       }
+
+       public ActionResult Search(BarefootProximityModel model)
+        {
+            // Re-initialize Search Results.
+            model.SearchResults = new List<string>();
+
+            // Does the Barefoot Proximity Items Session variable exist?
+            if (null != Session["BarefootProximityItems"])
+            {
+                // Yes - Retrieve the stored Barefoot Proximity Items from the Session variable.
+                model.Items = (List<string>)Session["BarefootProximityItems"];
+
+            }
+
+            // Does the model Search Criteria contain a value?
+            if (!string.IsNullOrEmpty(model.SearchCriteria))
+            {
+                // Yes - Retrieve the Items that contain the Search Criteria.
+                //  Items will be returned regardless of case
+                model.SearchResults = model.Items.Where(i => i.ToLower().Contains(model.SearchCriteria.ToLower())).ToList();
+            }
+
+            // Open the Search with the updated model.
+            return View("/Views/BarefootProximity/Search.cshtml", model);
+        }
+    }
+}
---
+++ DevAssignment/Models/BarefootProximityModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DevAssignment.Models
+{
+    public class BarefootProximityModel
+    {
+        #region Properties
+        public ModelStateDictionary ModelState { get; set; } = new ModelStateDictionary();
+
+        [Display(Name = "Create List of Strings to Search (comma delimited)")]
+        [DataType(DataType.MultilineText)] 
+        [Required(ErrorMessage = "Please enter a comma delimited list of search strings.")]
+        public string SearchList { get; set; } = "Lovelace, Hamilton, Hopper, Gates, Torvalds, Gosling, Eich, Crockford, Dahl"; 
+
+        public List<string> Items { get; set; } = new List<string>();
+
+        [Display(Name = "Enter Search Criteria")]
+        public string SearchCriteria { get; set; } = string.Empty;
+
+        public List<string> SearchResults { get; set; } = new List<string>();
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Default costructor. Assumes that properties will be set externally.
+        /// </summary>
+        public BarefootProximityModel()
+        {
+            // Set default model properties.
+        }
+        #endregion
+    }
+}
